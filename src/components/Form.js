87abb1eeaa1caeb10dd24019538c04c9055bb59f
Jobsidney <jobsidney67@gmail.com>
2022-07-16 11:31:25 +0300

    DOne

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([])
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -11,13 +12,35 @@ function Form(props) {
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
+  const handleSubmitChange=(event)=>{
+    event.preventDefault();
+    const formData={
+      firstName:firstName,
+      lastName:lastName,
+    }
+    const dataArray = [...submittedData, formData];
+    setSubmittedData(dataArray)
+    setFirstName(event.target.reset())
+    console.log(submittedData);
+
+  }
+  const listOfPeople = submittedData.map((person ,index)=>{
+    return(
+      <div key={index}>
+        {person.firstName} {person.lastName}
+      </div>
+    )
+  })
 
   return (
-    <form>
+    <div>
+    <form onSubmit={handleSubmitChange}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    {listOfPeople}
+    </div>
   );
 }
 
